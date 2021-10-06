0cfde01fe5bbf8e90dd2605983bd347deb1403b5
justussundelof <justus.sundelof@student.kyh.se>
2021-10-06 20:36:55 +0200

    finished to-do list

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from './Button'
+
+const Header = ( {onAdd, showAdd } ) => {
+   
+    return (
+        <header className='header'>
+            <h1>to-do list</h1>
+            <Button 
+                color={showAdd ? 'red' : 'green'} 
+                text={showAdd ? 'Close' : 'Add'} 
+                onClick={onAdd} 
+            />
+        </header>
+    )
+}
+
+export default Header
